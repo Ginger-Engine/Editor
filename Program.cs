f8a5023ee26258bfe29465bcac2a176553516836
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-07-06 19:13:27 +0300

    editor

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Engine.Core;
+
+var project = new Project();
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+project.Configure("resources/config/project.yaml");
+project.Init();
+project.Run();
