f8796b60cffdff5de7a36bc602290993b5c724b7
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-07-19 17:43:30 +0300

    pointer event system

---
+++ Ui/PointerEventHandlerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using Engine.Core.Behaviours;
+using Engine.Core.Entities;
+using Engine.Input.PointerEvents;
+using Engine.Rendering.RaylibBackend.Labels;
+
+namespace Ginger.Editor.Ui;
+
+public class PointerEventHandlerBehaviour(PointerInputManager pointerInputManager) : IEntityBehaviour
+{
+    public void OnStart(Entity entity)
+    {
+        pointerInputManager.Subscribe<EnterEvent>(entity, _ =>
+        {
+            entity.Modify((ref LabelComponent label) =>
+            {
+                label.Color = Color.Blue;
+            });
+        });
+        pointerInputManager.Subscribe<LeaveEvent>(entity, _ =>
+        {
+            entity.Modify((ref LabelComponent label) =>
+            {
+                label.Color = Color.Black;
+            });
+        });
+    }
+
+    public void OnDestroy(Entity entity)
+    {
+        
+        pointerInputManager.Unsubscribe<EnterEvent>(entity);
+        pointerInputManager.Unsubscribe<LeaveEvent>(entity);
+    }
+}
